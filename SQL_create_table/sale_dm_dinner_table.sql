20cbdfbc7f7792d958dcc9998d34a1d73aaef6c8
namxuan97 <nam.nx@joonsolutions.com>
2022-11-16 10:16:18 +0700

    add sale metadata run

---
+++ SQL_create_table/sale_dm_dinner_table.sql
@@ -0,0 +1,12 @@
+create table `pacc-raw-data.IPOS_SALE.dm_dinner_table` (
+PR_KEY	float64,
+DINNER_TABLE_ID	string,
+AREA_ID	string,
+DINNER_TABLE_NAME	string,
+DINNER_TABLE_TYPE	int64,
+ENABLE	int64,
+ACTIVE	int64,
+USER_ID	string,
+WORKSTATION_ID	int64,
+WHOS	string,
+)
