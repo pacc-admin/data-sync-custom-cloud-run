c9b520b29b4c53e365d08df4f9e8fd75d8fa4b55
namxuan97 <nam.nx@joonsolutions.com>
2022-11-16 09:13:50 +0700

    add final adjustmnet for deployment

--- sale-mssql-bq.py
+++ sale-mssql-bq.py
@@ -3,7 +3,12 @@ import pyodbc
 from google.cloud import bigquery
 import os
 
-#Setting BQ project in environment
+#Setting Sql server credential in environment
+server=os.environ.get("MSSQL_SALE_IP_ADDRESS")
+username=os.environ.get("MSSQL_SALE_IP_USERNAME")
+password=os.environ.get("MSSQL_SALE_IP_PASSWORD")
+
+#Setting BQ credential in environment
 os.environ.setdefault("GCLOUD_PROJECT", 'pacc-raw-data')
 service_account_file_path=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
@@ -11,12 +16,8 @@ class mssql_bq:
     #SQL Server
     def connect_to_mssql(self):
         print('step 1')
-        server = '180.93.172.30'
-        username = 'namba' 
-        password = '32UvRL9Om@VH'
         conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';PORT=1433;UID='+username+';PWD='+ password+';TrustServerCertificate=yes;')
         self.conn = conn
-        print('end step 1')
         
     def mssql_query_pd(self,query_string):
         print('step 2')
@@ -25,13 +26,19 @@ class mssql_bq:
         df = pd.DataFrame()    
         df = pd.read_sql(query, self.conn)
         self.dataframe = df
-        print('end step 2')
+
+        #checking
+        self.dataframe = df
+        if df.to_dict('records')==[]:
+            result='end'
+        else:
+            result='continue'
+        return result
     
     #BQ
     def connect_to_bq(self):
         print('step 3')
         self.client = bigquery.Client.from_service_account_json(service_account_file_path)
-        print('end step 3')
 
     def bq_insert(self,dataset_name):
         print('step 4')
@@ -50,33 +57,33 @@ class mssql_bq:
                 table.num_rows, len(table.schema), table_id
             )
         )
-        print('end step 4')
 
-database = ['IPOSS5WINE']
-#,' IPOSSBGN'
-#Sale
-#if __name__ == '__main__':
-#    for database_name in database:
-#        print(database_name)
-#        dataset_name = 'sale_detail'
-#        print(dataset_name)
-#        
-#        query_string = '''SELECT top 1
-#                    a.*,
-#                    b.workstation_name
-#                 
-#                from '''+database_name+'''.dbo.sale a
-#                left join '''+database_name+'''.dbo.dm_workstation b
-#                    on a.workstation_id = b.workstation_id
-#                    where 1=1
-#                '''
-#
-#        s = mssql_bq()
-#        s.connect_to_mssql()
-#        s.mssql_query_pd(query_string)
-#        #s.connect_to_bq()
-#        #s.bq_insert(dataset_name)
+database = ['IPOSS5WINE','IPOSSBGN']
 
+if __name__ == '__main__':
+    for database_name in database:
+        print(database_name)
+        dataset_name = 'sale'
+        print(dataset_name)
+        
+        query_string = '''SELECT
+                    a.*,
+                    b.workstation_name
+                 
+                from '''+database_name+'''.dbo.sale a
+                left join '''+database_name+'''.dbo.dm_workstation b
+                    on a.workstation_id = b.workstation_id
+                    where dateadd(day, datediff(day, 0, tran_date), 0) = dateadd(day,-1,dateadd(day, datediff(day, 0, getdate()), 0))
+                '''
+
+        s = mssql_bq()
+        s.connect_to_mssql()
+        s.mssql_query_pd(query_string)
+        if s.mssql_query_pd(query_string)=='end':
+            print('stop')
+        else:
+            s.connect_to_bq()
+            s.bq_insert(dataset_name)
 
 #Sale_detail
 if __name__ == '__main__':
@@ -95,7 +102,7 @@ if __name__ == '__main__':
                         from '''+database_name+'''.dbo.sale sale
                         left join '''+database_name+'''.dbo.dm_workstation store
                             on sale.workstation_id = store.workstation_id
-                        where year(tran_date) <= 2019
+                        where dateadd(day, datediff(day, 0, tran_date), 0) = dateadd(day,-1,dateadd(day, datediff(day, 0, getdate()), 0))
                     )
                     
                     select
@@ -108,10 +115,11 @@ if __name__ == '__main__':
                     inner join sale
                     on sale_detail.fr_key = sale.pr_key
                 '''
-        print(query_string)
         s = mssql_bq()
         s.connect_to_mssql()
         s.mssql_query_pd(query_string)
-        s.connect_to_bq()
-        s.bq_insert(dataset_name)
-
+        if s.mssql_query_pd(query_string)=='end':
+            print('stop')
+        else:
+            s.connect_to_bq()
+            s.bq_insert(dataset_name)
--- test.py
+++ test.py
@@ -1,25 +1,61 @@
+import pandas as pd
+import pyodbc
 from google.cloud import bigquery
 import os
+
+#Setting Sql server credential in environment
+server=os.environ.get("MSSQL_SALE_IP_ADDRESS")
+username=os.environ.get("MSSQL_SALE_IP_USERNAME")
+password=os.environ.get("MSSQL_SALE_IP_PASSWORD")
+
+#Setting BQ project in environment
 os.environ.setdefault("GCLOUD_PROJECT", 'pacc-raw-data')
 service_account_file_path=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-client = bigquery.Client.from_service_account_json(service_account_file_path)
-table_id='pacc-raw-data.test1.test_bq'
+class mssql_bq:
+    #SQL Server
+    def connect_to_mssql(self):
+        print('step 1')
+        conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';PORT=1433;UID='+username+';PWD='+ password+';TrustServerCertificate=yes;')
+        self.conn = conn
+        print('end step 1')
+        
+    def mssql_query_pd(self,query_string):
+        print('step 2')
+        query = query_string
+        
+        df = pd.DataFrame()    
+        df = pd.read_sql(query, self.conn)
+        self.dataframe = df
+        if df.to_dict('records')==[]:
+            result='end'
+        else:
+            result='continue'
+        return result
 
-rows_to_insert = [
-    {"title": "this is ery"}
-]
+    
+database = ['IPOSS5WINE']
 
-errors = client.insert_rows_json(table_id, rows_to_insert)  # Make an API request.
-if errors == []:
-    print("New rows have been added.")
-else:
-    print("Encountered errors while inserting rows: {}".format(errors))
+if __name__ == '__main__':
+    for database_name in database:
+        print(database_name)
+        dataset_name = 'sale'
+        print(dataset_name)
+        
+        query_string = '''SELECT
+                    a.*,
+                    b.workstation_name
+                 
+                from '''+database_name+'''.dbo.sale a
+                left join '''+database_name+'''.dbo.dm_workstation b
+                    on a.workstation_id = b.workstation_id
+                    where dateadd(day, datediff(day, 0, tran_date), 0) = dateadd(day,-1,dateadd(day, datediff(day, 0, getdate()), 0))
+                '''
 
-
-
-
-
-
-
-
+        s = mssql_bq()
+        s.connect_to_mssql()
+        s.mssql_query_pd(query_string)
+        if s.mssql_query_pd(query_string)=='end':
+            print('stop')
+        else:
+            print(s.connect_to_mssql())
