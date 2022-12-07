9a6de495b40e6d7bc9187872afa604008a2afe31
namxuan97 <nam.nx@joonsolutions.com>
2022-12-07 17:22:06 +0700

    finalize list of modules

--- dbconnector/__init__.py
+++ dbconnector/__init__.py
@@ -1,2 +1,4 @@
 from .mssql import mssql_query_pd
 from .big_query import connect_to_bq, bq_delete, bq_insert
+from .base_vn import base_vn_connect,total_page
+from .pd_process import get_two_day_before,pd_update_latest
