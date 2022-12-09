7326804c0b7cc53300b37df7f80759fc95ca4b6f
namxuan97 <nam.nx@joonsolutions.com>
2022-12-09 09:14:15 +0700

    add membership info to the changes

--- .SQL_create_table/mssql/sale_dm_membership_type.sql
+++ .SQL_create_table/mssql/sale_dm_membership_type.sql
@@ -1,10 +1,10 @@
 create or replace table `pacc-raw-data.IPOS_SALE.dm_membership_type` (
-    membership_type_id string,
-    membership_type_name string,
-    active int64,
-    user_id string,
-    workstation_id int64,
-    membership_type_parent_id string,
-    point_to_amount float64,
-    is_once int64
+    MEMBERSHIP_TYPE_ID string,
+    MEMBERSHIP_TYPE_NAME string,
+    ACTIVE INT64,
+    USER_ID string,
+    WORKSTATION_ID int64,
+    MEMBERSHIP_TYPE_PARENT_ID string,
+    POINT_TO_AMOUNT float64,
+    IS_ONCE int64
 )
