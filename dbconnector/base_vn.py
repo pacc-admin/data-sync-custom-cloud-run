38a9d45f822390f1bef8cbe70c6367d66f1500f1
namxuan97 <nam.nx@joonsolutions.com>
2022-12-16 09:53:57 +0700

    update basevn package

--- dbconnector/base_vn.py
+++ dbconnector/base_vn.py
@@ -2,6 +2,7 @@ import yaml
 import time
 from datetime import datetime, timedelta
 import requests
+import re
 
 def etract_variable_yml(dictionary):
     a_yaml_file = open("credentials/base_vn_token.yml")
@@ -9,14 +10,17 @@ def etract_variable_yml(dictionary):
     token=parsed_yaml_file[dictionary]
     return token
 
-def base_vn_connect(app,component,updated_from=0,page=0):
+def base_vn_connect(app,component1,component2='list',updated_from=0,page=0):
+    samp= re.compile('[a-zA-z]+')
+    component1=samp.findall(component1)[0]
+    component2=samp.findall(component2)[0]
     access_token = etract_variable_yml(app)
     page_dict={'page':page}
     updated_from_dict={'updated_from':updated_from}
     p={**access_token,**updated_from_dict,**page_dict}
-
     print(p)
-    url="https://"+app+".base.vn/extapi/v1/"+component+"/list"
+    url="https://"+app+".base.vn/extapi/v1/"+component1+"/"+component2
+    print(url)
     raw_output = requests.get(url, params=p).json()
     return raw_output
 
