aba70037734bd97985eb2a720d7285d6a0e1e4ba
namxuan97 <nam.nx@joonsolutions.com>
2022-12-18 21:14:34 +0700

    add tax create table

---
+++ .SQL_create_table/basevn/hrm/tax.sql
@@ -0,0 +1,17 @@
+create or replace table `pacc-raw-data.BASEVN_HRM.tax` (
+    id string,
+    type string,
+    metatype string,
+    code string,
+    user_id string,
+    username string,
+    name string,
+    content string,
+    since string,
+    last_update string,
+    status string,
+    color string,
+    percent string,
+    eba string,
+    config_percent string
+)
