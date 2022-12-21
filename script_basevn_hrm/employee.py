069bef7288c900350b5c079ddb4a60f3a9fe3354
namxuan97 <nam.nx@joonsolutions.com>
2022-12-21 09:38:42 +0700

    finalize employee

---
+++ script_basevn_hrm/employee.py
@@ -0,0 +1,23 @@
+import sys, os
+from google.cloud import bigquery
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
+import base_vn
+
+stop_words=['form']
+variable='employee'
+schema='BASEVN_HRM'
+job_config_list = bigquery.LoadJobConfig(
+    schema = [ 
+               bigquery.SchemaField("profile_marital",bigquery.enums.SqlTypeNames.STRING),
+            ]
+
+)
+query_string='select max(last_update) as last_update from `pacc-raw-data.'+schema+'.'+variable+'`'
+
+a=base_vn.while_loop_page_insert(app='hrm',
+                                 schema=schema,
+                                 column_name=variable,
+                                 job_config=job_config_list,
+                                 query_string_incre=query_string,
+                                 stop_words=stop_words
+                                 )
