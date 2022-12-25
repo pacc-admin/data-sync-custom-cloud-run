228738d3ab3acc441af841e6aea2d698946eeb26
namxuan97 <nam.nx@joonsolutions.com>
2022-12-25 18:36:17 +0700

    update sh file

--- bash_scheduling/base_vn_run.sh
+++ bash_scheduling/base_vn_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd pacc-data-sync-custom
 date +"%d-%m-%y-%T"
 for py_file in $(find script_base_vn* -name '*.py')
 do
     echo $py_file
-    python3 $py_file
+    #python3 $py_file
 done
--- bash_scheduling/mssql_sale_run.sh
+++ bash_scheduling/mssql_sale_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd pacc-data-sync-custom
 date +"%d-%m-%y-%T"
 for py_file in $(find script_mssql_sale -name '*.py')
 do
