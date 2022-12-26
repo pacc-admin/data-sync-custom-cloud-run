64c0d857c8d238d0de2e891c703ef761e90eb6d2
namxuan97 <nam.nx@joonsolutions.com>
2022-12-26 22:15:52 +0700

    update cycle

--- .SQL_create_table/basevn/payroll/cycle.sql
+++ .SQL_create_table/basevn/payroll/cycle.sql
@@ -1,13 +1,14 @@
 create or replace table `pacc-raw-data.BASEVN_PAYROLL.cycle` (
-  id string,
-  user_id string,
-  username string,
-  schedule string,
-  start_date string,
-  end_date string,
-  report_month string,
-  report_year string,
-  since string,
-  last_update string,
-  system_id string,
-  )
+    id string,
+    user_id string,
+    username string,
+    schedule string,
+    start_date string,
+    end_date string,
+    report_month string,
+    report_year string,
+    since string,
+    last_update string,
+    system_id string,
+    loaded_date timestamp
+)
