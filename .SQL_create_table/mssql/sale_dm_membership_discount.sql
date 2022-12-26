a9c33244cf964143421dae33d9aa8054b56f5f8c
namxuan97 <nam.nx@joonsolutions.com>
2022-12-26 14:24:49 +0700

    update sql file for dm membership discount

---
+++ .SQL_create_table/mssql/sale_dm_membership_discount.sql
@@ -0,0 +1,10 @@
+create or replace table `pacc-raw-data.IPOS_SALE.dm_membership_discount` (
+    MEMBERSHIP_TYPE_ID	string,
+    MEMBERSHIP_TYPE_NAME	string,
+    ACTIVE	int64,
+    USER_ID	string,
+    WORKSTATION_ID	int,
+    MEMBERSHIP_TYPE_PARENT_ID	string,
+    POINT_TO_AMOUNT	float64,
+    IS_ONCE	int64
+)
