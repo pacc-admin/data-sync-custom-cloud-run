d69f067366afb52a1c66fa802167e8e69c8cfbb1
namxuan97 <nam.nx@joonsolutions.com>
2022-12-26 14:10:13 +0700

    update mssql package

--- dbconnector/mssql.py
+++ dbconnector/mssql.py
@@ -1,6 +1,10 @@
+from big_query import connect_to_bq,bq_insert,bq_delete
 import pyodbc
 import pandas as pd
 import os
+
+#connect to bq
+client=connect_to_bq()
 
 def mssql_query_pd(server,username,password,query_string):
     conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';PORT=1433;UID='+username+';PWD='+ password+';TrustServerCertificate=yes;')
@@ -10,3 +14,17 @@ def mssql_query_pd(server,username,password,query_string):
     df = pd.read_sql(query, conn)
     dataframe = df
     return dataframe
+
+
+def full_refresh_sale(query_string,schema,table_id):
+    server=os.environ.get("MSSQL_SALE_IP_ADDRESS")
+    username=os.environ.get("MSSQL_SALE_IP_USERNAME")
+    password=os.environ.get("MSSQL_SALE_IP_PASSWORD")
+
+    print('step 1')
+    dataframe = mssql_query_pd(server,username,password,query_string)
+    print('step 2')
+    print('delete current table')
+    bq_delete(client,schema,table_id)
+    print('step 3')
+    bq_insert(client,schema,table_id,dataframe)
