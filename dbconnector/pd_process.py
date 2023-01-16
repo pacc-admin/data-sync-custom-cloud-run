17bf4e91428f7de48e58e0ae074d6068958755c2
namxuan97 <nam.nx@joonsolutions.com>
2023-01-16 16:32:15 +0700

    add pd process for nested schemas

--- dbconnector/pd_process.py
+++ dbconnector/pd_process.py
@@ -12,3 +12,10 @@ def pd_update_latest(dataset,last_update):
     final_dataset = dataset[dataset[last_update].astype('float') > get_two_day_before()]
     return final_dataset
                        
+def pd_nested_schema(df,column,mode='normalized'):
+    if mode=='normalized':
+      processed_df=pd.json_normalize(df[column])
+    else:
+      processed_df=df.explode(column)[column].apply(pd.Series).drop(columns=0,axis=1)
+    processed_df.columns = column+'_' + processed_df.columns
+    return processed_df
