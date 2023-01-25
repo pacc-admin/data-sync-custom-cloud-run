4cc26c21024e8941aea7e7d87da90507923a24df
namxuan97 <nam.nx@joonsolutions.com>
2023-01-25 21:58:22 +0700

    finalize daily sale targets

---
+++ script_google_sheet/daily_sale_targets.py
@@ -0,0 +1,17 @@
+import sys, os
+from google.cloud import bigquery
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
+
+from google_sheet import gg_sheet_import
+import big_query
+
+
+schema='CURRENT_TARGET'
+table_id='daily_sale_targets'
+
+with gg_sheet_import('target_daily') as s:
+    big_query.bq_delete(schema,table_id)
+    dataframe=s.sheet_to_pd()
+    print(dataframe.head(10))
+    big_query.bq_insert(schema,table_id,dataframe)
+
