1686c3ea1022c9d6adaf1dd0d2a22dbf6479108d
namxuan97 <nam.nx@joonsolutions.com>
2023-01-28 00:33:42 +0700

    add base vn & base checkin package

--- dbconnector/base_vn.py
+++ dbconnector/base_vn.py
@@ -1,64 +1,25 @@
-from big_query import bq_insert,bq_delete,bq_pandas
-from pd_process import pd_last_update
+from big_query import bq_insert,bq_delete
+from pd_process import pd_last_update,pd_type_change
 from google.cloud import bigquery
-import yaml
-from datetime import datetime, timedelta
-import requests
 import pandas as pd
 import inflect
+from base_vn_api import base_vn_connect_hiring,base_vn_connect_hrm_payroll
 
-
-p = inflect.engine()
-
-def etract_variable_yml(dictionary):
-    a_yaml_file = open("credentials/base_vn_token.yml") 
-    parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
-    token=parsed_yaml_file[dictionary]
-    return token
-
-def base_vn_connect(app,component1,component2='list',updated_from=0,page=0,para1='',value1='',para2='',value2=''):
-    #parameter delcare
-    access_token=etract_variable_yml(app)
-    page_dict={'page':page}
-    updated_from_dict={'updated_from':updated_from}
-    h = {"Content-type": "application/x-www-form-urlencoded"}
-    print(para2)
-
-    #combine into dictionary
-    if para1=='' and para2=='':
-        p={**access_token,**updated_from_dict,**page_dict}
-    elif para1!='' and para2=='':
-        p={**access_token,**updated_from_dict,**page_dict,**{para1:value1}}
-    elif para2!='' and para1=='':
-        p={**access_token,**updated_from_dict,**page_dict,**{para2:value2}}
-    else:
-        p={**access_token,**updated_from_dict,**page_dict,**{para1:value1},**{para2:value2}}
-
-    if component2 !='':
-        try:
-            url="https://"+app+".base.vn/extapi/v1/"+component1+"/"+component2
-            tester=requests.get(url, params=p).json()     
-            if tester['data']!='':
-                raw_output = tester
-            else:
-                url="https://"+app+".base.vn/publicapi/v2/"+component1+"/"+component2
-                raw_output = requests.post(url, headers=h, data=p).json()
-        except:
-            url="https://"+app+".base.vn/publicapi/v2/"+component1+"/"+component2
-            raw_output = requests.post(url, headers=h, data=p).json()
-    else:
-        url="https://"+app+".base.vn/extapi/v1/"+component1
-        raw_output = requests.post(url, headers=h, data=p).json()
+def base_vn_connect(app,component1,component2='list',updated_from=0,page=0,para1='',value1=''):
+    if app=='hiring':
+        raw_output=base_vn_connect_hiring(component1,component2=component2,updated_from=updated_from,page=page,para1=para1,value1=value1)
     
+    else:
+        raw_output=base_vn_connect_hrm_payroll(component1,app,component2=component2,updated_from=updated_from,page=page)
+        
     return raw_output
 
-def pd_process(
+def pd_flatten(
         raw_output,
         column_to_flat,
-        query_string_incre,
-        stop_words=[],
         url_component2='list'
     ):
+    p = inflect.engine()
     #flatten dataset currently nested in array column
     if url_component2=='list':
         cp=p.plural(column_to_flat)
@@ -66,28 +27,38 @@ def pd_process(
         cp=url_component2
     dataset = pd.DataFrame(raw_output)
     flatten = pd.json_normalize(dataset[cp])
+    return flatten
+
+def pd_process(
+        dataset,
+        query_string_incre,
+        stop_words=[]
+    ):
 
     #filter by last update date in BQ
-    final_dataset=pd_last_update(df=flatten,query_string_incre=query_string_incre)
+    final_dataset=pd_last_update(df=dataset,query_string_incre=query_string_incre)
 
     #rename schema with '.' to '_' 
-    a=final_dataset.filter(like='.')
     final_dataset.columns = final_dataset.columns.str.replace(".", "_", regex=True) 
 
     #remove specified word from a list of column, for later data type change
-    pd_process.pd_type_change(df=final_dataset,columns=stop_words)
+    pd_type_change(df=final_dataset,columns=stop_words)
 
     #add loaded date field
     final_dataset['loaded_date'] = pd.to_datetime('today')
+    print(final_dataset)
 
     return final_dataset
                        
 
-def total_page(raw_output):
+def total_page(raw_output,total_items='total_items',items_per_page='items_per_page'):
     try:
         r=raw_output
-        total_items=int(r['total_items'])
-        items_per_page=int(r['items_per_page'])
+        total_items=int(r[total_items])
+        try:
+            items_per_page=int(r[items_per_page])
+        except:
+            items_per_page=50
         total_page=total_items/items_per_page
     except:
         total_page=0 
@@ -108,12 +79,14 @@ def while_loop_page_insert(app,
                            para1='',
                            value1='',
                            job_config= bigquery.LoadJobConfig(),
-                           stop_words=[]
+                           stop_words=[],
+                           total_items='total_items',
+                           items_per_page='items_per_page'
                         ):
     #specify variable
-    pageno=-1
+    pageno=0
     r=base_vn_connect(app=app,component1=column_name,component2=component2,para1=para1,value1=value1)
-    total_page_display=total_page(r)
+    total_page_display=total_page(r,total_items,items_per_page)
 
     #regulate table name from components
     if component2=='list':
@@ -121,37 +94,45 @@ def while_loop_page_insert(app,
     else:
         table_id=column_name+'_'+component2
 
-    #loop pageno until it reach total page firgure
-    while pageno < total_page_display:
-        pageno=pageno+1
-        r=base_vn_connect(app=app,component1=column_name,page=pageno,component2=component2,para1=para1,value1=value1)
-        try:    
-            data_to_insert= pd_process(
-                                    raw_output=r,
-                                    column_to_flat=column_name,
-                                    query_string_incre=query_string_incre,
-                                    stop_words=stop_words,
-                                    url_component2=component2
-                            )
+    if total_page_display<=1:
+        dataset=pd_flatten(raw_output=r,column_to_flat=column_name,url_component2='list')
+
+    else:
+        dataset=pd.DataFrame()
+
+        print('start loop')
+        while pageno < total_page_display:
+            pageno=pageno+1
+            print(pageno)
+            r=base_vn_connect(app=app,component1=column_name,page=pageno,component2=component2,para1=para1,value1=value1)
+            dataset_raw=pd_flatten(raw_output=r,column_to_flat=column_name,url_component2='list')
+            dataset=pd.concat([dataset,dataset_raw])
     
-            #stop if inserted objects is empty
-            if data_to_insert.to_dict('records')==[]:
-                print('end')
-            else:
-                print('continue')
-    
-                #remove column with id matches the inserted rows from basevn
-                print(data_to_insert['id'])
-                row_to_exclude="('"+"','".join(data_to_insert['id'].to_list())+"')"
-                condition='id in'+row_to_exclude
-                bq_delete(schema,table_id,condition=condition)
-    
-                bq_insert(
-                    schema,
-                    table_id=table_id,
-                    dataframe=data_to_insert,
-                    job_config=job_config
-                )
-                print('end')
-        except:
+    try:    
+        data_to_insert= pd_process(
+                                    dataset,
+                                    query_string_incre,
+                                    stop_words=stop_words
+                                )
+
+        #stop if inserted objects is empty
+        if data_to_insert.to_dict('records')==[]:
             print('end')
+        else:
+            print('continue')
+
+            #remove column with id matches the inserted rows from basevn
+            print(data_to_insert['id'])
+            row_to_exclude="('"+"','".join(data_to_insert['id'].to_list())+"')"
+            condition='id in'+row_to_exclude
+            bq_delete(schema,table_id,condition=condition)
+
+            bq_insert(
+                schema,
+                table_id=table_id,
+                dataframe=data_to_insert,
+                job_config=job_config
+            )
+            print('end')
+    except:
+        print('end')
---
+++ dbconnector/base_vn_checkin.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import requests
+import time
+import pandas as pd
+from google.cloud import bigquery
+import big_query,pd_process
+from base_vn_checkin import get_base_checkin_api
+
+class base_vn_checkin:
+    def __init__(self):
+        print('init method called')
+             
+    def __enter__(self):
+        print('enter method called')
+        return self
+         
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print('exit method called')
+
+    
+    def tranform_logs_to_df(self,query_string):
+        raw_output = get_base_checkin_api(query_string)
+        logs=[sub['logs'] for sub in raw_output['logs']]
+        
+        total=[]
+        for i in range(len(logs)):
+          b=list(logs[i].values())
+          total=total+b
+        
+        self.df=pd.DataFrame(total)
+        
+        return self.df
+            
+    def df_process(self):
+        #nomalize dictionary columns
+        lists=['finalized','computed','stats']
+        for item in lists:
+          self.df=self.df.join(pd_process.pd_nested_schema(self.df,item)).drop(columns=item,axis=1)
+        
+        #flatten multi dict columns
+        self.df=self.df.join(pd_process.pd_nested_schema(self.df,'logs',mode='flatten')).drop(columns='logs',axis=1)
+        
+        #add loaded date
+        self.df['loaded_date'] = pd.to_datetime('today')
+        
+        #convert type
+        ##string
+        non_converted_column_str=[
+            'date',
+            'month_index',
+            'timesheet_id',
+            'computed_is_late',
+            'computed_day_point',
+            'computed_sum_minute_late',
+            'computed_sum_late',
+            'finalized_is_late',
+            'finalized_day_point',
+            'finalized_sum_minute_late',
+            'finalized_sum_late',
+            'logs_checkout',
+            'logs_time',
+            'loaded_date'
+        ]
+        self.df=pd_process.pd_type_change(self.df,columns=non_converted_column_str)
+
+        ##int64
+        converted_columns_int=[
+            'date',
+            'month_index',
+            'logs_time'
+        ]
+        self.df=pd_process.pd_type_change(self.df,columns=converted_columns_int,converted_type=int,type='include')
+
+    def incremental_update(self,query_string):
+        self.final_dataset=pd_process.pd_last_update(self.df,query_string,column_updated='logs_time')
+        print(self.final_dataset.dtypes)
+        print(self.final_dataset)
+    
+    def bq_batch_load(self,schema,table_id):
+        job_config_list = bigquery.LoadJobConfig(
+                schema=[
+                    bigquery.SchemaField("id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("user_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("employee_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("date",bigquery.enums.SqlTypeNames.INT64),
+                    bigquery.SchemaField("month_index",bigquery.enums.SqlTypeNames.INT64),
+                    bigquery.SchemaField("timesheet_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("hid",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("token",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("type",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("stats_comments",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_checkout",bigquery.enums.SqlTypeNames.FLOAT64),
+                    bigquery.SchemaField("logs_client_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_content",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_files",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_img",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_ip",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_lat",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_lng",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_metatype",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_note",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_office_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_photo",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("logs_time",bigquery.enums.SqlTypeNames.INT64),
+                    bigquery.SchemaField("logs_office_id",bigquery.enums.SqlTypeNames.STRING),
+                    bigquery.SchemaField("loaded_date",bigquery.enums.SqlTypeNames.TIMESTAMP),
+                    
+                  ]
+        )
+        big_query.bq_insert(schema,table_id,dataframe=self.final_dataset,job_config=job_config_list)
