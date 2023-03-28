1a6b78973c6eb92bff34d70fbd5b7ff4a14d5d11
nvjrepo <103881752+nvjrepo@users.noreply.github.com>
2023-03-28 07:36:20 +0700

    Update big_query.py

--- dbconnector/big_query.py
+++ dbconnector/big_query.py
@@ -84,7 +84,7 @@ def bq_latest_date(date_schema,schema,table_id):
     #finding latest date from BQ table
     df=bq_pandas(query_string='select max(cast('+date_schema+' as date)) as '+date_schema+' from `pacc-raw-data.'+schema+'.'+table_id+'`')
     print(df)
-    if pd.isnull(np.datetime64('NaT'))==True:
+    if df[date_schema].astype(str).to_list()[0]=='NaT':
         recent_loaded_date='1970-01-01'
     else:
         recent_loaded_date=df[date_schema].astype(str).to_list()[0]
