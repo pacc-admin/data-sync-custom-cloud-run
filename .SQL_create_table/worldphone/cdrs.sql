53f50dc1ad466a13d41799393958834fe0af2cd1
nam-97 <nam.nx@joonsolutions.com>
2023-04-04 17:06:13 +0700

    add initial table file

---
+++ .SQL_create_table/worldphone/cdrs.sql
@@ -0,0 +1,16 @@
+create or replace table `pacc-raw-data`.WORLDFONE.cdrs (
+    calldate string,
+    src string,
+    dst string,
+    queue string,
+    disposition string,
+    duration int64,
+    billsec int64,
+    holdtime int64,
+    waitingtime int64,
+    uniqueid string,
+    accountcode string,
+    did_number string,
+    direction string,
+    carrier string
+)
