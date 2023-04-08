731b05573250e1b17ce55fba0cde1305eb1afcbb
nam-97 <nam.nx@joonsolutions.com>
2023-04-08 10:49:23 +0700

    print date column

--- dbconnector/mssql.py
+++ dbconnector/mssql.py
@@ -9,13 +9,13 @@ from datetime import timedelta
 def mssql_query_pd(server,username,password,query_string):
     conn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';PORT=1433;UID='+username+';PWD='+ password+';TrustServerCertificate=yes;')
     query = query_string
-    print(query)
     df = pd.DataFrame()    
     df = pd.read_sql(query, conn)
     dataframe = df
 
     #convert date column to datetime
     date_cols = [col for col in df.columns if 'date' in col.lower()]
+    print(date_cols)
     for col in date_cols: 
        df[col] = pd.to_datetime(df[col], format='%d/%m/%y %H:%M:%S')
 
