94a1c86689e1e46985c6a1f054b40326dbf80ffe
nam-97 <nam.nx@joonsolutions.com>
2023-04-14 08:38:45 +0700

    add shifts

---
+++ script_base_vn_schedule/shift.py
@@ -0,0 +1,23 @@
+import sys, os
+from google.cloud import bigquery
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
+
+import base_vn
+
+variable='shift'
+schema='BASEVN_SCHEDULE'
+
+job_config_list = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("loaded_date",bigquery.enums.SqlTypeNames.TIMESTAMP)
+        ]
+)
+
+query_string='select max(last_update) as last_update from `pacc-raw-data.'+schema+'.'+variable+'`'
+print(query_string)
+a=base_vn.while_loop_page_insert(app='schedule',
+                                 schema=schema,
+                                 column_name=variable,
+                                 job_config=job_config_list,
+                                 query_string_incre=query_string
+                                 )
