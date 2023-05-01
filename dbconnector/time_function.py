0fe127e38f7f224e589441ab586613cd1e9dd922
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-01 10:28:16 +0700

    fix worldphone

--- dbconnector/time_function.py
+++ dbconnector/time_function.py
@@ -3,10 +3,23 @@ import time
 from dateutil.relativedelta import relativedelta
 import dateutil.relativedelta
 
-def last_unix_t_of_month(unix_dt):
+def convert_unix_to_date(unix_dt):
     dt = datetime.fromtimestamp(unix_dt)
     date_dt=dt.date()
+    return date_dt
+
+def last_date_of_month(unix_dt):
+    date_dt=convert_unix_to_date(unix_dt)
     last_date=date_dt+ relativedelta(day=31)
+    return last_date
+
+def unix_month_no(unix_dt):
+    date_dt=convert_unix_to_date(unix_dt)
+    month_number = date_dt.month
+    return month_number
+
+def last_unix_t_of_month(unix_dt):
+    last_date=last_date_of_month(unix_dt)
     last_date_dt=datetime.strptime(str(last_date)+' 23:59:59', "%Y-%m-%d %H:%M:%S")
     
     last_date_unix= int(time.mktime(last_date_dt.timetuple()))
@@ -14,8 +27,7 @@ def last_unix_t_of_month(unix_dt):
     return last_date_unix
 
 def first_unix_t_of_last_month(unix_dt):
-    dt = datetime.fromtimestamp(unix_dt)
-    date_dt=dt.date()
+    date_dt=convert_unix_to_date(unix_dt)
     first_date=date_dt.replace(day=1)
     last_month_date = first_date + dateutil.relativedelta.relativedelta(months=-1)
     last_month_date_dt=datetime.strptime(str(last_month_date)+' 00:00:00', "%Y-%m-%d %H:%M:%S")
--- dbconnector/worldfone.py
+++ dbconnector/worldfone.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime
 from big_query import bq_insert,bq_delete,bq_pandas
 from yml_extract import etract_variable_yml_string
-from time_function import last_unix_t_of_month
+from time_function import last_unix_t_of_month,convert_unix_to_date,unix_month_no,last_date_of_month
 
 
 def get_worldfone_api(startdate,enddate,page=1):
@@ -21,7 +21,7 @@ def worldfone_pd(start_date,end_date):
     data_to_insert=pd.DataFrame()
     raw_output = get_worldfone_api(startdate=start_date,enddate=end_date)
     total_pages=raw_output['max_page']+1
-    print(total_pages)
+    print('Total page is '+str(total_pages))
 
     #concat df to all pages data
     for page in range(1,total_pages):
@@ -36,11 +36,46 @@ def worldfone_bq(schema,table_id):
     query_string="select max(unix_seconds(timestamp(calldate || ' UTC+7'))) as calldate FROM "+'`pacc-raw-data.'+schema+'.'+table_id+'`'
     start_date=bq_pandas(query_string)['calldate'].astype(int).to_list()[0] + 1
     end_date = int(time.mktime(datetime.today().timetuple()))
+
+    if unix_month_no(end_date)==unix_month_no(start_date):
+        end_date=end_date
+    else:
+        end_date=last_unix_t_of_month(start_date)
+
     data_to_insert=worldfone_pd(start_date,end_date=end_date)
 
     if data_to_insert.to_dict('records')==[]:
-        result='No Insert'
-        print('end')
+        
+        
+        if last_date_of_month(start_date)==convert_unix_to_date(start_date) and end_date==last_unix_t_of_month(start_date):
+                
+                #if last date and end date fall into last date of month => switch range to next date of month
+                start_date=end_date+1
+                end_date=last_unix_t_of_month(start_date)
+                data_to_insert=worldfone_pd(start_date,end_date=end_date)
+
+                if data_to_insert.to_dict('records')==[]:
+                    result='No Insert'
+                    print('end')
+                
+                else:
+                    print('continue')
+                    #remove column with id matches the inserted rows from basevn
+                    unique_key=data_to_insert['uniqueid']+data_to_insert['direction']
+                    row_to_exclude="('"+"','".join(unique_key.to_list())+"')"
+                    condition='concat(uniqueid,direction) in'+row_to_exclude
+            
+                    result=bq_insert(
+                                schema,
+                                table_id,
+                                dataframe=data_to_insert,
+                                condition=condition
+                            )
+                    print('end')
+
+        else:
+            result='No Insert'
+            print('end')
         
     else:
         print('continue')
