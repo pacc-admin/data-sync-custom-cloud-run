e8b1926559317aa002162825bed83ef7f495be25
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-05 08:47:08 +0700

    add test crm files

---
+++ script_ipos_crm/membership_insert_test.py
@@ -0,0 +1,14 @@
+import sys, os
+import pandas as pd
+from google.cloud import bigquery
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
+import ipos_crm_flow,big_query
+
+brand='BGN'
+schema='dbo'
+table='member_vouchers'
+field_to_update='update_at'
+
+df = ipos_crm_flow.membership_data(brand, condition=' limit 1')
+user_id_list=df['membership_id'].to_list()
+print(user_id_list)
