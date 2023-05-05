7a405d6e810d3cf2b48d8678104b121a30f3db04
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-05 08:54:21 +0700

    add crm packages

--- dbconnector/ipos_crm_flow.py
+++ dbconnector/ipos_crm_flow.py
@@ -1,56 +1,15 @@
 from big_query import bq_insert_streaming,bq_query,bq_pandas,bq_insert
+from mssql import mssql_query_pd
 from pd_process import pd_last_update
 import requests
 import pandas as pd
 import time
 
-def create_membership_table(brand):
-    query_string='''
-            create or replace table `pacc-raw-data.dbo._tmp_membership_check` as (
-                with membership_id_sale as (
-                  SELECT distinct membership_id FROM `pacc-raw-data.IPOS_SALE.sale` WHERE data_source = 'IPOSS'''+brand+''''
-                ),
-                
-                member_vouchers as (
-                  select
-                      membership_id,
-                      max(datetime(date_created)) as date_created
-                  
-                  from pacc-raw-data.IPOS_CRM_'''+brand+'''.member_vouchers
-                  group by 1
-                ),
-
-                member_rating as (
-                  select
-                      membership_id,
-                      max(datetime(created_at)) as created_at
-                  
-                  from pacc-raw-data.IPOS_CRM_'''+brand+'''.member_rating
-                  group by 1
-                )
-                
-                select
-                    '''+"'"+brand+"'"+''' as brand,
-                    membership_id_sale.membership_id,
-                    coalesce(datetime(membership_details.update_at),datetime('1970-01-01')) as update_at,
-                    coalesce(member_vouchers.date_created,datetime('1970-01-01')) as date_created,
-                    coalesce(member_rating.created_at,datetime('1970-01-01')) as created_at
-                
-                from membership_id_sale
-                left join pacc-raw-data.IPOS_CRM_'''+brand+'''.membership_detail membership_details
-                    on membership_id_sale.membership_id = membership_details.membership_id
-                left join member_vouchers
-                    on member_vouchers.membership_id = membership_id_sale.membership_id
-                left join member_rating
-                    on member_rating.membership_id = membership_id_sale.membership_id
-            )
-    '''
-    print(query_string)
-    bq_query(query_string)
-
 def membership_data(brand,condition=''):
-    query_string='select * from `pacc-raw-data.dbo._tmp_membership_check` where brand='+"'"+brand+"'"+condition
-    membership_data=bq_pandas(query_string)
+    database_name='IPOSS'+brand
+    query_string='select distinct membership_id from '+database_name+'.dbo.sale '+condition
+    membership_data=mssql_query_pd(query_string)
+    print('Total members are '+str(len(membership_data.index)))
     return membership_data
 
 def crm_api(brand,user_id,table,page=0):
@@ -70,18 +29,14 @@ def crm_api(brand,user_id,table,page=0):
     
     return r
     
-def crm_insert(brand,user_id,table,field_to_update,o1='',o2=''):
+def crm_transform(brand,user_id,schema,table,field_to_update,o1='',o2=''):
     r=crm_api(brand,user_id,table)
-    schema='IPOS_CRM_'+brand
-    table_id='pacc-raw-data.'+schema+'.'+table
 
     #specify updating dimension
     user_filter='and membership_id='+"'"+user_id+"'"
-    try:
-        updated_field=pd.to_datetime(membership_data(brand,condition=user_filter)[field_to_update]).to_list()[0]
-    except:
-        updated_field=pd.to_datetime('1970-01-01')
-
+    query_string_incre='select max(unix_seconds(timestamp('+field_to_update+'))) as '+field_to_update+'''
+                           from `pacc-raw-data.'''+schema+'''.'''+table+'` where membership_id='+"'"+user_id+"'"
+    
     #convert to pandas dataframe
     if r!=0:
         try:
@@ -98,29 +53,26 @@ def crm_insert(brand,user_id,table,field_to_update,o1='',o2=''):
             else:
                 dataframe=pandas_convert
             dataframe['membership_id']=user_id
-            dataframe['loaded_date']=time.time()
-
+            print(dataframe) 
+        
             #update only changes entries
-            dataframe=dataframe[pd.to_datetime(dataframe[field_to_update])>updated_field]
+            print('start update')
+            dataframe==pd_last_update(dataframe,query_string_incre,field_to_update)
+
+            dataframe['loaded_date'] = pd.to_datetime('today')
             if o2=='':
                 dataframe=dataframe
             else:
                 dataframe=dataframe[dataframe[o2]>0]
-            rows_to_insert=dataframe.to_dict('records')
-            print(rows_to_insert)
-
-            #insert to BQ
-            try:
-                bq_insert_streaming(rows_to_insert=rows_to_insert,table_id=table_id,object='membership_id='+user_id+', brand='+brand)
-            except:
-                table_id=table
-                bq_insert(schema,table_id,dataframe)
             
         except:
-            print('stop\n')
+            dataframe=pd.DataFrame()
 
     else:
-        print('stop\n')
+        dataframe=pd.DataFrame()
+    
+    print(dataframe)    
+    return dataframe
 
 def crm_insert_with_page(brand,user_id,table,field_to_update,o1='',o2=''):
     r=crm_api(brand,user_id,table)
@@ -132,7 +84,28 @@ def crm_insert_with_page(brand,user_id,table,field_to_update,o1='',o2=''):
 
     while pageno < total_page:
         pageno=pageno+1
-        crm_insert(brand,user_id,table,field_to_update,o1,o2)
+        crm_transform(brand,user_id,table,field_to_update,o1,o2)
+
+def crm_insert(brand,user_id,schema,table,field_to_update):
+    df = membership_data(brand)
+    user_id_list=df['membership_id'].to_list()
+    
+    print('insert table '+table)
+    
+    dataframe=pd.DataFrame()
+    for user_id in user_id_list:
+        print('start with member_id:'+user_id)
+        dataframe_user_id=crm_insert(brand,user_id,schema,table,field_to_update)
+        dataframe=pd.concat([dataframe,dataframe_user_id])
+    
+    print(dataframe)
+    job_config_list = bigquery.LoadJobConfig(
+            schema=[
+                bigquery.SchemaField("loaded_date",bigquery.enums.SqlTypeNames.TIMESTAMP)
+            ]
+    )
+    big_query.bq_insert(schema,table,dataframe,job_config=job_config_list)
+
 
 def crm_campaigns_insert(brand):
     #variable
