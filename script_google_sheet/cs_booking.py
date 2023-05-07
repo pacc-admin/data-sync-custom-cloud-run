9269c8c2e59d1f1626e8690339900f68b94148b7
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-07 18:07:15 +0700

    fix coniditon to booking file

--- script_google_sheet/cs_booking.py
+++ script_google_sheet/cs_booking.py
@@ -11,6 +11,6 @@ sheet_names=['FW','BGNK','BGQT','BGLQD','BGSH','BGLVS','BGBC']
 column_to_clean='booking_date'
 
 with gg_sheet_import('Booking system') as s:
-    big_query.bq_delete(schema,table_id)
+    big_query.bq_delete(schema,table_id,condition='true')
     dataframe=s.sheet_to_pd_name(sheet_names,column_to_clean)
     big_query.bq_insert(schema,table_id,dataframe)
