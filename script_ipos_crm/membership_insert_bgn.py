7fd5bd6dea947ae79f6e3cc298fbccaa452019cd
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-08 08:26:29 +0700

    fixing crm schema

--- script_ipos_crm/membership_insert_bgn.py
+++ script_ipos_crm/membership_insert_bgn.py
@@ -1,10 +1,11 @@
 import sys, os
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
-import ipos_crm_flow
+import ipos_crm_flow,big_query
 
 brand='BGN'
 table='member_vouchers'
 field_to_update='update_at'
 
+big_query.bq_delete(schema,table_id,condition='true')
 ipos_crm_flow.crm_insert(brand,table,field_to_update)
