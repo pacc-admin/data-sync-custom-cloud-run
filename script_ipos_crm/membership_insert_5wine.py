c31a819d41401cfbd4bff5a20c6226bb5c588fb3
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-05-09 06:48:00 +0700

    adjust schema

--- script_ipos_crm/membership_insert_5wine.py
+++ script_ipos_crm/membership_insert_5wine.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
 import ipos_crm_flow
 
 brand='5WINE'
+schema='IPOS_CRM_'+brand
 table='member_vouchers'
 field_to_update='update_at'
 
--- script_ipos_crm/membership_insert_bgn.py
+++ script_ipos_crm/membership_insert_bgn.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
 import ipos_crm_flow,big_query
 
 brand='BGN'
+schema='IPOS_CRM_'+brand
 table='member_vouchers'
 field_to_update='update_at'
 
