f20eca63779e4f8bed9f00332bd466f97006297a
nvjrepo <103881752+nvjrepo@users.noreply.github.com>
2023-05-10 09:34:31 +0700

    Update dm_membership_discount.py

--- script_mssql_sale/dm_membership_discount.py
+++ script_mssql_sale/dm_membership_discount.py
@@ -7,9 +7,7 @@ database = ['IPOSS5WINE','IPOSSBGN']
 schema='IPOS_SALE'
 table_name='dm_membership_discount'
 
-print('delete current table')
-big_query.bq_delete(schema,table_id=table_name)
-
 for database_name in database:
+    condition="data_source='"+database_name+"'"
     query_string = "select *, "+"'"+database_name+"'"+' as data_source from '+database_name+'.dbo.'+table_name
     mssql.full_refresh_sale(query_string,schema,table_id=table_name)
