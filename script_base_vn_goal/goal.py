fcc6e5d61c1e7d8687fb685d75c4ce3b39b8ba65
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-06-08 10:06:53 +0700

    update goal

--- script_base_vn_goal/goal.py
+++ script_base_vn_goal/goal.py
@@ -2,17 +2,15 @@ import sys, os
 from google.cloud import bigquery
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
 
-import base_vn_api,big_query,dict_function
-import json
+import base_vn_api,big_query
 
 app='goal'
+metric='goal'
 schema='BASEVN_GOAL'
 table='goal'
-column_updated='last_update'
-unique_key='id'
 
 #execute
-source_output=base_vn_api.get_base_goal_api(app)
+source_output=base_vn_api.get_base_goal_metric_api(app,metric)
 big_query.full_refresh_bq_insert_from_json(source_output,schema,table_id=table)
 
 
