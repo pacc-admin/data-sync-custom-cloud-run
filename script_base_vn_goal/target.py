52458076faf9ad188a6d7207e0ce34f697d48ab3
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-06-08 10:06:21 +0700

    add target

---
+++ script_base_vn_goal/target.py
@@ -0,0 +1,14 @@
+import sys, os
+from google.cloud import bigquery
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../dbconnector")
+
+import base_vn_api,big_query
+
+app='goal'
+metric='target'
+schema='BASEVN_GOAL'
+table='target'
+
+#execute
+source_output=base_vn_api.get_base_goal_metric_api(app,metric)
+big_query.full_refresh_bq_insert_from_json(source_output,schema,table_id=table)
