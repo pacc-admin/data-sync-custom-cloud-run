8321d0ce0380116a961e15472d1793a8d027e866
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-08-01 08:01:43 +0700

    fix membership id

--- dbconnector/ipos_crm_flow.py
+++ dbconnector/ipos_crm_flow.py
@@ -41,11 +41,11 @@ def crm_get_full_list(brand,table,page=0):
     for user_id in user_id_list:
         print('get data for member_id:'+user_id)
         raw_output_member=crm_api(brand,user_id,table,page=0)
-        raw_output_member['membership_id']=user_id
 
         if raw_output_member==0:
             print('no data')
         else:
+            raw_output_member['membership_id']=user_id
             if type(raw_output_member) is dict:
                 raw_output.append(raw_output_member)
             else:
