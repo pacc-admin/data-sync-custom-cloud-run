26b5d9854a17c8e2cfed0fd428fbaa9c8daaff56
nvjrepo <namxuan97@Nams-MacBook-Air.local>
2023-10-16 08:21:26 +0700

    fix

--- script_base_vn_ehiring/contact.py
+++ script_base_vn_ehiring/contact.py
@@ -22,39 +22,58 @@ job_config_list = bigquery.LoadJobConfig(
         ]
 )
 
-df=pd.DataFrame()
 
-for pool_id in pool_ids:
-    print(pool_id)
-    query_string='''
-                    select max(last_update) as last_update 
-                    from `pacc-raw-data.'''+schema+'.'+variable+'`'+'''
-                    where ns_id='''+"'"+pool_id+"'"
-    
-    
-    df_pool=base_vn.while_loop_page_return(app='hiring',
-                                     column_name=variable,
-                                     query_string_incre=query_string,
-                                     para1='pool_id',
-                                     value1=pool_id,
-                                     component2=component2,
-                                     stop_words=stop_words
-                                     )
-    df = pd.concat([df,df_pool])
+def get_data():
+    df=pd.DataFrame()
+    for pool_id in pool_ids:
+        print(pool_id)
+        query_string='''
+                        select max(last_update) as last_update 
+                        from `pacc-raw-data.'''+schema+'.'+variable+'`'+'''
+                        where ns_id='''+"'"+pool_id+"'"
+        
+        
+        df_pool=base_vn.while_loop_page_return(app='hiring',
+                                         column_name=variable,
+                                         query_string_incre=query_string,
+                                         para1='pool_id',
+                                         value1=pool_id,
+                                         component2=component2,
+                                         stop_words=stop_words
+                                         )
+        df = pd.concat([df,df_pool])
+    return df
 
-#condition to exclude
+def condition(df):
+    #condition to exclude
+    try:
+        condition='id in '+"('"+"','".join(df['id'].to_list())+"')"
+    except:
+        condition='true'
+    return condition
+
 try:
-    condition='id in '+"('"+"','".join(df['id'].to_list())+"')"
+    #insert to BQ
+    result=big_query.bq_insert(
+                schema,
+                table_id=variable,
+                dataframe=get_data(),
+                condition=condition(get_data()),
+                job_config=job_config_list,
+                unique_key='id'
+            )
 except:
-    condition='true'
-
-#insert to BQ
-result=big_query.bq_insert(
-            schema,
-            table_id=variable,
-            dataframe=df,
-            condition=condition,
-            job_config=job_config_list,
-            unique_key='id'
-        )
+    print("recreate table")
+    query_string = 'create or replace table '+schema+'.'+variable+' (id string, loaded_date timestamp)'
+    big_query.bq_query(query_string)    
+    result=big_query.bq_insert(
+                schema,
+                table_id=variable,
+                dataframe=get_data(),
+                condition=condition(get_data()),
+                job_config=job_config_list,
+                unique_key='id'
+            )
+    
 print(result)
+
